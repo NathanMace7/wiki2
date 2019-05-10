2e450d27e86eaf6270805428dfdbb0b011d0ead8
Nathan Mace <403980@edu.cwa.ac.uk>
2019-05-10 15:56:11 +0100

    first commit

---
+++ mysite/wiki/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=64, primary_key=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('wiki:detail', args=[self.title])
---
+++ mysite/wiki/templatetags/wiki_tags.py
@@ -0,0 +1,19 @@
+from django import template
+import markdown
+import re
+
+wikiWord = re.compile(r"\[\[([A-Za-z0-9_]+)\]\]")
+
+register = template.Library()
+
+
+@register.filter
+def markup(text):
+    return markdown.markdown(text)
+
+
+@register.filter
+def wikify(text):
+    return wikiWord.sub(r'''
+    <a href="/wiki/\1/">\1</a>
+    ''', text)
