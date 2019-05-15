b724581d0d1fbdd839c4fbe68633a757453f9e0e
Nathan Mace <403980@edu.cwa.ac.uk>
2019-05-15 15:04:29 +0100

    Added File Uploads

--- mysite/wiki/urls.py
+++ mysite/wiki/urls.py
@@ -5,15 +5,15 @@ from . import views
 
 app_name = 'wiki'
 urlpatterns = [
-    #path('accounts/login/' LoginView.as_view(), name='login')
-    #path('accounts/logout/' LogoutView.as_view(), name='logout')
     path('', views.IndexView.as_view(), name='index'),
+    path('upload/', views.upload_file, name='upload_page' ), 
     path('<str:pk>/edit', views.edit_page, name='edit_page'),
     path('<str:pk>/save', views.save_page, name='save_page'),
     path('<str:pk>/', views.view_page, name='detail'),
-    path('<str:pk>/upload/', views.upload_file, name='upload_page' ),    
+       
 ]
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
