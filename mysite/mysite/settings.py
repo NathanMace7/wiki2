01e96c5f7fa758c04839149911a12c1a38a7209c
Nathan Mace <403980@edu.cwa.ac.uk>
2019-05-24 09:21:44 +0100

    Improved Top Button

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,10 +133,6 @@ LOGOUT_REDIRECT_URL = "/wiki/accounts/login/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = '/wiki/'
-LOGOUT_REDIRECT_URL = '/wiki/accounts/login'
-AUTHOR = '403980'
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
