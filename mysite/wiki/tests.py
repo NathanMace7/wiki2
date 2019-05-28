4d2675c9a9a87b43a2d6d62eebfafdda8dca28e6
Nathan Mace <403980@edu.cwa.ac.uk>
2019-05-28 11:24:39 +0100

    Improved Testing

--- mysite/wiki/tests.py
+++ mysite/wiki/tests.py
@@ -1,39 +1,30 @@
 import datetime
 
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 from django.utils import timezone
 from django.urls import reverse
-
 from .models import Page
 
 class PageCreationTestCase(TestCase):
     def setUp(self):
-        page = Page.objects.create(title ="Hello World A", content = "hello world a")
+        page = Page.objects.create(title="Page_A", content="This is Page A for Testing")
         page.save()
 
-        page = Page.objects.create(title = "Hello World B", content = "hello world b")
+        page = Page.objects.create(title="Page_B", content="This is Page B for Testing")
         page.save()
 
     def test_hello_world_a(self):
-        response = self.client.get('/wiki/Hello World A/')
-        self.assertContains(response, 'hello world a')
+        response = self.client.get('/wiki/Page_A/')
+        self.assertContains(response, 'This is Page A for Testing')
 
     def test_hello_world_b(self):
-        response = self.client.get('/wiki/Hello World B/')
-        self.assertContains(response, 'hello world b')
+        response = self.client.get('/wiki/Page_B/')
+        self.assertContains(response, 'This is Page B for Testing')
 
     def test_page_non_existant(self):
         response = self.client.get('/wiki/IAmFailing/')
         self.assertContains(response, 'As this page does not exist, would you like to create it?')
-
-    def test_edit_me_later(self):
-        page = Page.objects.create(title = "editing", content = "Im going to be changing soon")
-        page.save()
-
-    def test_edit_page(self):
-        response = self.client.get('/wiki/editing/edit/')
-        print('TESTING', response.content)
-        self.assertContains(response, 'Editing')
 
 class PageLoadTestCase(TestCase):
 
@@ -48,114 +39,28 @@ class PageLoadTestCase(TestCase):
     def test_login_view(self):
         response = self.client.get('/wiki/accounts/login/')
         self.assertContains(response, 'Username:')
-# class QuestionModelTests(TestCase):
 
-#     def test_was_published_recently_with_future_question(self):
-#         """
-#         was_published_recently() returns False for questions whose pub_date
-#         is in the future.
-#         """
-#         time = timezone.now() + datetime.timedelta(days=30)
-#         future_question = Question(pub_date=time)
-#         self.assertIs(future_question.was_published_recently(), False)
+class LoginTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('TestUser', 'TestUser@gmail.com', 'testingpassword')
 
-#     def test_was_published_recently_with_old_question(self):
-#         """
-#         was_published_recently() returns False for questions whose pub_date
-#         is older than 1 day.
-#         """
-#         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-#         old_question = Question(pub_date=time)
-#         self.assertIs(old_question.was_published_recently(), False)
+    def testLogin(self):
+        self.client.login(username='TestUser', password='testingpassword')
+        response = self.client.get(reverse('wiki:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Logout")
 
-#     def test_was_published_recently_with_recent_question(self):
-#         """
-#         was_published_recently() returns True for questions whose pub_date
-#         is within the last day.
-#         """
-#         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-#         recent_question = Question(pub_date=time)
-#         self.assertIs(recent_question.was_published_recently(), True)
+class EditPageTest(TestCase):
+    def test_edit_me_later(self):
+        page = Page.objects.create(title = "editing", content = "Im going to be changing soon")
+        page.save()
 
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('TestUser', 'TestUser@gmail.com', 'testingpassword')
 
-# class QuestionIndexViewTests(TestCase):
-#     def test_no_questions(self):
-#         """
-#         If no questions exist, an appropriate message is displayed.
-#         """
-#         response = self.client.get(reverse('polls:index'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, "No polls are available.")
-#         self.assertQuerysetEqual(response.context['latest_question_list'], [])
-
-#     def test_past_question(self):
-#         """
-#         Questions with a pub_date in the past are displayed on the
-#         index page.
-#         """
-#         create_question(question_text="Past question.", days=-30)
-#         response = self.client.get(reverse('polls:index'))
-#         self.assertQuerysetEqual(
-#             response.context['latest_question_list'],
-#             ['<Question: Past question.>']
-#         )
-
-#     def test_future_question(self):
-#         """
-#         Questions with a pub_date in the future aren't displayed on
-#         the index page.
-#         """
-#         create_question(question_text="Future question.", days=30)
-#         response = self.client.get(reverse('polls:index'))
-#         self.assertContains(response, "No polls are available.")
-#         self.assertQuerysetEqual(response.context['latest_question_list'], [])
-
-#     def test_future_question_and_past_question(self):
-#         """
-#         Even if both past and future questions exist, only past questions
-#         are displayed.
-#         """
-#         create_question(question_text="Past question.", days=-30)
-#         create_question(question_text="Future question.", days=30)
-#         response = self.client.get(reverse('polls:index'))
-#         self.assertQuerysetEqual(
-#             response.context['latest_question_list'],
-#             ['<Question: Past question.>']
-#         )
-
-#     def test_two_past_questions(self):
-#         """
-#         The questions index page may display multiple questions.
-#         """
-#         create_question(question_text="Past question 1.", days=-30)
-#         create_question(question_text="Past question 2.", days=-5)
-#         response = self.client.get(reverse('polls:index'))
-#         self.assertQuerysetEqual(
-#             response.context['latest_question_list'],
-#             ['<Question: Past question 2.>', '<Question: Past question 1.>']
-#         )
-
-
-# class QuestionDetailViewTests(TestCase):
-#     def test_future_question(self):
-#         """
-#         The detail view of a question with a pub_date in the future
-#         returns a 404 not found.
-#         """
-#         future_question = create_question(
-#             question_text='Future question.', days=5)
-#         url = reverse('polls:detail', args=(future_question.id,))
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, 404)
-
-#     def test_past_question(self):
-#         """
-#         The detail view of a question with a pub_date in the past
-#         displays the question's text.
-#         """
-#         past_question = create_question(
-#             question_text='Past Question.', days=-5)
-#         url = reverse('polls:detail', args=(past_question.id,))
-#         response = self.client.get(url)
-#         self.assertContains(response, past_question.question_text)
-# '''
+    def test_edit_page(self):
+        self.client.login(username='TestUser', password='testingpassword')
+        response = self.client.post('/wiki/editing/edit', follow=True)
+        self.assertContains(response, 'Editing')
